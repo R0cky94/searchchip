347b6b1d3318aa3c4683fabc145b67dfb74f441f
Rakesh GR <rakesh.gr@codematrix.org>
2019-06-04 16:04:18 +0530

    initial committed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Chips from 'react-chips'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const languages = ["Cricket", "VollyBall", "Football", "Tennis", "Boxing", "Archer"
+    , "Ufc", "Swimming", "Batminton", "Races"];
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chips: []
+        }
+    }
+
+    onChange = chips => {
+        this.setState({chips});
+    };
+
+    render() {
+
+        return (
+            <div className="App">
+                <div className="chipContainer">
+                    <Chips
+                        placeholder="Please select your favourite sports"
+                        value={this.state.chips}
+                        onChange={this.onChange}
+                        suggestions={languages}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
